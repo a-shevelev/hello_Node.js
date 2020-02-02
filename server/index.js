6ca9a5a9b86c05e08b212e79340228d82ea832e6
a-shevelev <60062720+a-shevelev@users.noreply.github.com>
2020-02-02 15:11:59 +0300

    custom complete

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 let express = require("express");
+let cors = require("cors");
 let app = express();
+
+app.use(cors());
 
 app.get("/", function (request, response) {
 	response.send("Hello, Node.js!");
@@ -7,7 +10,7 @@ app.get("/", function (request, response) {
 
 app.get("/page", function(req, res){
 	let x = req.query.foo;
-	res.send({foo: 2 * x});
+	res.send({foo: 2 * x})
 })
 
 app.listen(591);
